7eb71ddb292f0ae92cdce32cf8d6ea0c96f3ca4f
Shahzil-Imran <166384172+Shahzil-Imran@users.noreply.github.com>
2025-12-21 14:51:51 +0500

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -131,4 +131,5 @@ async def analyze_pet(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
